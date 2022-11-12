d98ee9e4dbbe7c4090b00007d61718956994d67b
marchin7 <114770259+marchin7@users.noreply.github.com>
2022-11-12 00:52:31 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const card1 = document.getElementById('card1');
 const card2 = document.getElementById('card2');
 const rateElement = document.getElementById('rate');
@@ -6,11 +7,21 @@ const buttons = document.querySelectorAll('.btn');
 buttons.forEach(btn => {btn.addEventListener('click', rate)
 
 function rate() {
-   rateElement.innerText = btn.value
+   rateElement.innerText = btn.value;
 }
 })
 
 function submit() {
-    card1.style.display = 'none'
-    card2.style.display = 'flex'
+    
+    if (rateElement.innerText === '') {
+        card2.style.display ='none'
+        card1.style.display = 'flex'
+        alert('Please select a number')
+    }
+    
+    else {
+        card1.style.display = 'none'
+        card2.style.display = 'flex'
+    }
 }
+
