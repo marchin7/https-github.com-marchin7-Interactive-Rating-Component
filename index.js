300dcfb7cdbce7337febcdd43f0670359317e91e
marchin7 <114770259+marchin7@users.noreply.github.com>
2023-02-19 21:23:02 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
-const form = document.getElementById('form');
-const card1 = document.getElementById('card1');
-const card2 = document.getElementById('card2');
-const rateElement = document.getElementById('rate');
-const buttons = document.querySelectorAll('.btn');
-const submitBtn = document.getElementById('submit-btn');
-const alertMsg = 'Please select a rating';
+const form = document.getElementById("form");
+const card1 = document.getElementById("card1");
+const card2 = document.getElementById("card2");
+const rateElement = document.getElementById("rate");
+const buttons = document.querySelectorAll(".btn");
+const submitBtn = document.getElementById("submit-btn");
+const alertMsg = "Please select a rating";
 
-form.addEventListener('submit', (e)=>{
+form.addEventListener("submit", (e) => {
     e.preventDefault();
 });
 
-buttons.forEach(item=>{item.addEventListener('click', ()=>{
-    buttons.forEach(btn=>{
-        if(item !== btn) {
-            btn.classList.remove('active')
-        }
-        else {btn.classList.add('active')}
-    })
-    rateElement.innerText = item.innerText;
-})});
-
-submitBtn.addEventListener('click', ()=>{
-    if (rateElement.innerText === '') {
-        card2.style.display = 'none';
-        card1.style.display = 'flex';
-        alert(alertMsg);
-    }
-    else {
-        card1.style.display = 'none';
-        card2.style.display = 'flex';
-    }
+buttons.forEach((item) => {
+    item.addEventListener("click", () => {
+        buttons.forEach((btn) => {
+            if (item !== btn) {
+                btn.classList.remove("active");
+            } else {
+                btn.classList.add("active");
+            }
+        });
+        rateElement.innerText = item.innerText;
+    });
 });
 
-
-
-
-
+submitBtn.addEventListener("click", () => {
+    if (rateElement.innerText === "") {
+        card2.style.display = "none";
+        card1.style.display = "flex";
+        alert(alertMsg);
+    } else {
+        card1.style.display = "none";
+        card2.style.display = "flex";
+    }
+});
